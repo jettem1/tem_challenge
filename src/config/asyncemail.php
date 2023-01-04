77bb92437170b1783574ac3f808530b82ce31cbb
Dan <jettem@proton.me>
2023-01-04 13:16:01 +0100

    Adding Sendgrid client library and configuration files

--- src/config/asyncemail.php
+++ src/config/asyncemail.php
@@ -2,8 +2,12 @@
 
 return [
     'mailjet' => [
-        'key' => env('MAILJET_KEY'),
-        'secret' => env('MAILJET_SECRET'),
-        'senderEmail' => env('MAILJET_SENDER_EMAIL'),
+        'key' => env('MAILJET_KEY', ''),
+        'secret' => env('MAILJET_SECRET', ''),
+        'senderEmail' => env('MAILJET_SENDER_EMAIL', ''),
+    ],
+    'sendgrid' => [
+        'apiKey' => env('SENDGRID_API_KEY', ''),
+        'senderEmail' => env('SENDGRID_SENDER_EMAIL', ''),
     ],
 ];
