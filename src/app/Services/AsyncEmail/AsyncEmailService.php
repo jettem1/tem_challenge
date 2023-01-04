11907e8ebb5cf6717a00c99377b13fcc60068cbb
Dan <jettem@proton.me>
2023-01-04 12:19:50 +0100

    Function for sending Email using one of the available transport methods

--- src/app/Services/AsyncEmail/AsyncEmailService.php
+++ src/app/Services/AsyncEmail/AsyncEmailService.php
@@ -5,11 +5,22 @@ namespace App\Services\AsyncEmail;
 use App\Exceptions\EmailException;
 use App\Models\Email;
 use App\Services\AsyncEmail\DataTransferObjects\OutgoingEmailDTO;
+use App\Services\AsyncEmail\Transport\IEmailTransport;
 use Illuminate\Support\Facades\DB;
 use PDOException;
 
 class AsyncEmailService
 {
+    /**
+     * @var array<IEmailTransport> List of email transports, ordered by preference (priority)
+     */
+    private array $emailTransportList = [];
+
+    public function addEmailTransport(IEmailTransport $emailTransport)
+    {
+        $this->emailTransportList[] = $emailTransport;
+    }
+
     /**
      * Stores the Email records in the database and
      * adds them to the Queueing system
@@ -46,6 +57,49 @@ class AsyncEmailService
         DB::commit();
 
         return $emailIds;
+    }
+
+    /**
+     * Sends the Email and updates the record status
+     *
+     * @param Email $email
+     * @return bool
+     * @throws EmailException
+     */
+    public function sendEmail(Email $email): bool
+    {
+        if ($email->status != Email::STATUS_PENDING) {
+            throw new EmailException('Email already processed, aborting.');
+        }
+
+        if (!count($this->emailTransportList)) {
+            throw new EmailException('No email transport options provided, aborting.');
+        }
+
+        $email->status = Email::STATUS_IN_PROGRESS;
+        $email->save();
+
+        /*
+         * Try to use all available transports until
+         * email is successfully sent
+         */
+        foreach ($this->emailTransportList as $emailTransport) {
+            $isSent = $emailTransport->send($email);
+            if ($isSent) {
+                $email->status = Email::STATUS_SENT;
+                $email->save();
+                return true;
+            }
+        }
+
+        /*
+         * Email could not be sent using any transport,
+         * mark it as failed
+         */
+        $email->status = Email::STATUS_FAILED;
+        $email->save();
+
+        return false;
     }
 
 }
