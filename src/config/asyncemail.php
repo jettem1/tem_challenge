5b80f4378fb515e6e45977ec3581548b3f40016a
Dan <jettem@proton.me>
2023-01-04 13:00:51 +0100

    Adding MailJet client library and configuration files/env. vars

---
+++ src/config/asyncemail.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'mailjet' => [
+        'key' => env('MAILJET_KEY'),
+        'secret' => env('MAILJET_SECRET'),
+        'senderEmail' => env('MAILJET_SENDER_EMAIL'),
+    ],
+];
